73f0ceadef5945b6c69b7fcef611d772ad0f5e93
Dylan Player <dylanplayer@Dylans-MacBook-Pro.local>
2021-09-15 23:25:36 -0700

    Added post functions

--- data.js
+++ data.js
@@ -3,20 +3,24 @@ const posts = [
     {
         "id": 1,
         "username": "John_Doe",
-        "image": "/posts/John_Doe/1",
+        "profileImage": "/profiles/John_Doe.png",
+        "image": "/posts/John_Doe/1.jpeg",
         "description": "This is my post about the Grand Canyon"
     },
     {
         "id": 2,
         "username": "John_Doe",
-        "image": "/posts/John_Doe/2",
+        "profileImage": "/profiles/John_Doe.png",
+        "image": "/posts/John_Doe/2.jpeg",
         "description": "This is my post about New York"
     },
     {
         "id": 3,
         "username": "John_Doe",
-        "profileImage": "/profiles/John_Doe",
-        "image": "/posts/John_Doe/3",
+        "profileImage": "/profiles/John_Doe.png",
+        "image": "/posts/John_Doe/3.jpeg",
         "description": "This is my post about San Francisco"
     }
 ]
+
+export default posts;
--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
-import posts from "./data";
+import posts from './data.js';
 
-const postContainer = document.querySelector();
 
-for(let post in posts){
+const postContainer = document.querySelector("#posts");
 
+
+for(let i = 0; i < posts.length; i++){
+    
     const postDiv = document.createElement("div");
     postDiv.className = "post";
 
@@ -12,12 +14,22 @@ for(let post in posts){
 
     const profileImage = document.createElement("img");
     profileImage.className = "profile-img";
-    profileImage.src = post.profileImage;
+    profileImage.src = posts[i].profileImage;
     postHeader.appendChild(profileImage);
+    
+    postDiv.appendChild(postHeader)
 
     const username = document.createElement("h2");
-    username.innerHTML = post.username;
+    username.className = "username";
+    username.innerHTML = posts[i].username;
+    postHeader.appendChild(username);
 
+    const postImage = document.createElement("img");
+    postImage.className = "post-image";
+    postImage.src = posts[i].image;
+    postDiv.appendChild(postImage);
+
+    postContainer.appendChild(postDiv);
 
 }
 
